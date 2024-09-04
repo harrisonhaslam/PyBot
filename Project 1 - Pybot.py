744bdbdd6e087e7c6f147e6f9cad95425683d5dc
Harrison Haslam <haslamharrison@gmail.com>
2024-09-03 19:31:21 -0600

    Add files via upload

--- Project 1 - Pybot.py
+++ Project 1 - Pybot.py
@@ -1,3 +1,5 @@
+# INTRO #
+
 print("Hello! I am your Python bot, PyBot!")
 user_name = input("What is your name? ")
 print(f"Nice to meet you, {user_name}!")
@@ -8,4 +10,19 @@ print(f"You are {age_difference} years older than me. I'm only {bot_age} years o
 color = input("What's your favorite color? ")
 print(f"Oh, {color} is a beautiful color!")
 random_color = ["red","blue","green","yellow","orange","purple"]
-print(f"My favorite color is {random_color[4]}")
+print(f"My favorite color is {random_color[4]}.")
+
+## JOKE SECTION ##
+
+print(f"Hey, {user_name}. Would you like to hear a joke?")
+
+joke = "Why did the python get promoted?"
+answer = "Because it always delivered “exceptional” results!"
+
+yes_no = input("Yes or No? ")
+
+if yes_no == "Yes" or "yes":
+    print(joke)
+    print(answer)
+else:
+    print("Well, that's a shame.")
